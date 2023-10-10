93030970c80d7d8ad63d67f10e1d72c06a0d382d
Random-Guy0 <amren.gill@gmail.com>
2023-10-10 15:11:35 +1100

    created process list for used applications

---
+++ Assets/Scripts/UI/Applications/ApplicationItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+
+public class ApplicationItem : MonoBehaviour
+{
+    private Process _process;
+    public Process Process
+    {
+        get => _process;
+        set
+        {
+            _process = value;
+            applicationNameText.SetText(_process.ProcessName);
+        }
+    }
+
+    [SerializeField] private TMP_Text applicationNameText;
+
+    public void Delete()
+    {
+        GameManager.Instance.Processes.Remove(Process);
+        Destroy(gameObject);
+    }
+}
