6efdc29e2246a713b8da2e378940b54d353a50dd
Random-Guy0 <amren.gill@gmail.com>
2023-10-12 17:23:19 +1100

    implemented tracking of mouse and keyboard input using unity raw input

--- Assets/Scripts/Managers/WorkManager.cs
+++ Assets/Scripts/Managers/WorkManager.cs
@@ -1,69 +1,84 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
+using UnityRawInput;
 
 public class WorkManager : MonoBehaviour
 {
     [SerializeField] private float checkPeriod = 1f;
 
-    private float _timer = 0f;
+    private float _timer;
 
-    private bool inputDetectedInPeriod = false;
+    private bool _inputDetectedInPeriod = false;
+
+    private int _secondsInFocus = 0;
+    private int _secondsOfInput = 0;
+
+    private void Start()
+    {
+        RawInput.Start();
+        RawInput.WorkInBackground = true;
+    }
+
+    private void OnDestroy()
+    {
+        RawInput.Stop();
+    }
 
     private void Update()
     {
-        CheckForInput();
-        
+        _inputDetectedInPeriod = CheckForInput();
+
         _timer += Time.deltaTime;
         if (_timer >= checkPeriod)
         {
             CheckForWork();
             _timer = 0f;
         }
+        
+        Debug.Log("Seconds in Focus: " + _secondsInFocus);
+        Debug.Log("Seconds of Input: " + _secondsOfInput);
     }
 
     private void CheckForWork()
     {
-        Debug.Log("Chosen Applications in focus: "+ FocusedWindowIsWork());
+        if (FocusedWindowIsWork())
+        {
+            _secondsInFocus++;
+            
+            if (_inputDetectedInPeriod)
+            {
+                _secondsOfInput++;
+            }
+        }
     }
 
-    private void CheckForInput()
+    private bool CheckForInput()
     {
-        //Keyboard keyboard = Keyboard.current;
-        //Mouse mouse = Mouse.current;
-        //Debug.Log(keyboard.anyKey.isPressed);
-        //if(keyboard.anyKey.isPressed || mouse.delta.magnitude > 0f)
+        return RawInput.AnyKeyDown;
     }
 
-    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    [DllImport("user32.dll")]
     private static extern IntPtr GetForegroundWindow();
-    
-    [System.Runtime.InteropServices.DllImport("user32.dll")]
-    private static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll")]
+    private static extern int GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
     private bool FocusedWindowIsWork()
     {
-        IntPtr hwnd = GetForegroundWindow();
-        if (hwnd == null)
-        {
-            return false;
-        }
+        var hwnd = GetForegroundWindow();
+        if (hwnd == null) return false;
 
-        GetWindowThreadProcessId(hwnd, out uint processId);
+        GetWindowThreadProcessId(hwnd, out var processId);
 
-        bool result = false;
+        var result = false;
 
-        foreach (Process p in GameManager.Instance.Processes)
-        {
+        foreach (var p in GameManager.Instance.Processes)
             if (p.Id == processId)
             {
                 result = true;
                 break;
             }
-        }
 
         return result;
     }
