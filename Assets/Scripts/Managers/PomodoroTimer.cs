2e3c2990986355ed1b96b9fd15e7d9fe18d47581
Amren Gill <amren.gill@gmail.com>
2023-11-03 14:14:58 +1100

    change debug time

--- Assets/Scripts/Managers/PomodoroTimer.cs
+++ Assets/Scripts/Managers/PomodoroTimer.cs
@@ -33,8 +33,8 @@ public class PomodoroTimer : MonoBehaviour
         if (debugMode)
         {
             workChunkDuration = 11f;
-            breakChunkDuration = 31f;
-            longBreakChunkDuration = 45f;
+            breakChunkDuration = 16f;
+            longBreakChunkDuration = 46f;
         }
     }
 
